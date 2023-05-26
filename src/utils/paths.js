c8286bf3bde008679b97beb15ec1c3ea50aeb483
Emerick Fouchard <fouchardemerick@gmail.com>
2023-05-26 12:34:32 +0200

    Ajout paths app/homepage

---
+++ src/utils/paths.js
@@ -0,0 +1,14 @@
+export const APP_PATHS = {
+    HOME: '/',
+    PROJECT: 'projects/:projectId',
+    ERROR_PAGE: '/error'
+};
+
+export const HOME_PATHS = {
+    HOME: '/#home',
+    ABOUT: '/#about',
+    SKILLS: '/#skills',
+    SERVICES: '/#services',
+    PORTFOLIO: '/#portfolio',
+    CONTACT: '/#contact'
+};
