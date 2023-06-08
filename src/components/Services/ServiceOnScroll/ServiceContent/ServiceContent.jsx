01d3067f54c2e6659aafbe23ea2889de6c7049e1
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-08 12:16:29 +0200

    Animation scroll parallax changement opacité ok

--- src/components/Services/ServiceOnScroll/ServiceContent/ServiceContent.jsx
+++ src/components/Services/ServiceOnScroll/ServiceContent/ServiceContent.jsx
@@ -8,20 +8,23 @@ import {
     useSpring
   } from "framer-motion";
 
-function useParallax(value, distance, distance2, distance3) {
-    return useTransform(value, [0, 0.5, 1], [distance, distance2, distance3]);
+function useParallax(value, position, distance) {
+    return useTransform(value, [0, 0.5, 1], [position, position, distance]);
 }
 
 function ServiceContent({ title, alt, src, desc }) {
     const ref = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: ref
-    });
-    const y = useParallax(scrollYProgress, 0, 0, -400);
+    const { scrollYProgress } = useScroll({ target: ref });
+    const y = useParallax(scrollYProgress, 0, -400);
+
+    const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1, 0])
 
     return (
         <div className={`img-desc-div ${styles.ImgDescDiv}`} ref={ref}>
-            <motion.img style={{ y }} src={src} alt={alt} />
+            <motion.img 
+                style={{ y, opacity: opacity }}
+                src={src}
+                alt={alt} />
             <article className={`service-description ${styles.Description}`} >
                 <motion.h2>{title}</motion.h2>
                 <p>{desc}</p>
