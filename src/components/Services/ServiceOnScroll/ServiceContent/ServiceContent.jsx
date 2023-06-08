c5715eca37c18789d9e0729f66105d282294946c
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-08 12:31:34 +0200

    Simplification code scroll animations

--- src/components/Services/ServiceOnScroll/ServiceContent/ServiceContent.jsx
+++ src/components/Services/ServiceOnScroll/ServiceContent/ServiceContent.jsx
@@ -8,21 +8,21 @@ import {
     useSpring
   } from "framer-motion";
 
-function useParallax(value, position, distance) {
-    return useTransform(value, [0, 0.5, 1], [position, position, distance]);
+function useParallax(value, outputValue1, outputValue2, outputValue3) {
+    return useTransform(value, [0, 0.5, 1], [outputValue1, outputValue2, outputValue3]);
 }
 
 function ServiceContent({ title, alt, src, desc }) {
     const ref = useRef(null);
     const { scrollYProgress } = useScroll({ target: ref });
-    const y = useParallax(scrollYProgress, 0, -400);
 
-    const opacity = useTransform(scrollYProgress, [0, 0.5, 1], [1, 1, 0])
+    const y = useParallax(scrollYProgress, 0, 0, -400);
+    const opacity = useParallax(scrollYProgress, 1, 1, 0);
 
     return (
         <div className={`img-desc-div ${styles.ImgDescDiv}`} ref={ref}>
             <motion.img 
-                style={{ y, opacity: opacity }}
+                style={{ y, opacity }}
                 src={src}
                 alt={alt} />
             <article className={`service-description ${styles.Description}`} >
