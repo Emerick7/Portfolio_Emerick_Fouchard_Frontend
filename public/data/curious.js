36ef870733d55a3b89ab9051014eb9cb156d9136
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-08 16:26:55 +0200

    curious

--- public/data/curious.js
+++ public/data/curious.js
@@ -1,5 +1,5 @@
 function curious() {
-    console.log("┻┳|\n┳┻| _\n┻┳| •.•)\n┳┻|⊂ﾉ\n┻┳|\n\nSalut 👋, curieux !\nVous vous demandez probablement à quel point ce site est cool.\n\nSi vous pensez que c'est intéressant et que vous voulez voir le code derrière,\nJe l'ai placé à l'air libre sur https://github.com/whizkydee/olaolu.dev.\nN'hésitez pas à y jeter un œil!\n\nAussi, vous pouvez me contacter sur https://twitter.com/mrolaolu ou via hello@olaolu.dev ✨.");
+    console.log("┻┳|\n┳┻| _\n┻┳| •.•)\n┳┻|⊂ﾉ\n┻┳|\n\nSalut 👋, curieux !\n\nVous vous demandez probablement comment ce site est aussi cool.\n\nSi vous pensez que c'est intéressant et que vous voulez voir le code qui est derrière ce site,\nje l'ai placé sur https://github.com/Emerick7/Portfolio_Emerick_Fouchard_Frontend.\n\nN'hésitez pas à y jeter un œil!\n\nAussi, vous pouvez me contacter sur https://twitter.com/FouchardEmerick ou via fouchardemerick@gmail.com ✨.");
 }
 
 export default curious;
