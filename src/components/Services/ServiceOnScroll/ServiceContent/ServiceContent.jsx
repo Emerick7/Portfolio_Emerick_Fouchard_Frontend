d2c01ff7ab3d053f41a47de6409f84c4db88c702
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-08 12:42:29 +0200

    Animations scroll services terminées

--- src/components/Services/ServiceOnScroll/ServiceContent/ServiceContent.jsx
+++ src/components/Services/ServiceOnScroll/ServiceContent/ServiceContent.jsx
@@ -8,26 +8,27 @@ import {
     useSpring
   } from "framer-motion";
 
-function useParallax(value, outputValue1, outputValue2, outputValue3) {
-    return useTransform(value, [0, 0.5, 1], [outputValue1, outputValue2, outputValue3]);
-}
-
 function ServiceContent({ title, alt, src, desc }) {
     const ref = useRef(null);
     const { scrollYProgress } = useScroll({ target: ref });
 
-    const y = useParallax(scrollYProgress, 0, 0, -400);
-    const opacity = useParallax(scrollYProgress, 1, 1, 0);
+    function useParallax(outputValue1, outputValue2, outputValue3) {
+        return useTransform(scrollYProgress, [0, 0.5, 1], [outputValue1, outputValue2, outputValue3]);
+    }
 
     return (
         <div className={`img-desc-div ${styles.ImgDescDiv}`} ref={ref}>
             <motion.img 
-                style={{ y, opacity }}
+                style={{ y: useParallax(0, 0, -400), opacity: useParallax(1, 1, 0) }}
                 src={src}
                 alt={alt} />
             <article className={`service-description ${styles.Description}`} >
-                <motion.h2>{title}</motion.h2>
-                <p>{desc}</p>
+                <motion.h2 style={{ y: useParallax(0, 0, 800), opacity: useParallax(1, 1, 0) }} >
+                    {title}
+                </motion.h2>
+                <motion.p style={{ y: useParallax(0, 0, 400), opacity: useParallax(1, 1, 0) }} >
+                    {desc}
+                </motion.p>
             </article>
         </div>
     );
