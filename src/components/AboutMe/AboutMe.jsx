5424e1f21f08db48b7b04f21cec99578a29a7508
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-12 12:49:11 +0200

    Card animation about me

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -3,46 +3,42 @@ import { delay, motion } from 'framer-motion';
 import aboutMePhoto from '../../assets/about-me-photo.svg';
 
 function AboutMe() {
+    const variants = {
+        offscreen: {
+          y: 0
+        },
+        onscreen: {
+          y: -230,
+          rotate: -5,
+          transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8,
+            delay: 0.5
+          }
+        }
+      };
+
     return (
-        <section className={styles.AboutMeSection}>
+        <motion.section
+            className={styles.AboutMeSection}
+            initial="offscreen"
+            whileInView="onscreen"
+            viewport={{ once: true, amount: 0.8 }}
+            >
+            <div className={styles.Splash}></div>
             <motion.article
                 className={styles.AboutMeBox}
-                initial={{
-                    scale: 1,
-                    rotate: 0,
-                    borderRadius: "0%"
-                }}
-                whileInView={{
-                    scale: [1, 0.5, 0.5, 1, 1, 1],
-                    rotate: [0, 0, 180, 180, 0, 0],
-                    borderRadius: ["30px", "0%", "50%", "50%", "1%", "30px"]
-                }}
-                transition={{
-                    duration: 2,
-                    delay: 1,
-                    ease: "easeInOut",
-                    times: [0, 0.2, 0.4, 0.6, 0.8, 1]
-                }}
+                variants={variants}
             >
-                <motion.img
-                    src={aboutMePhoto}
-                    alt="emerick-fouchard-about-me-developper-full-stack"
-                    initial={{ scale: 1 }}
-                    whileInView={{ scale: [1, 0.5, 0.5, 0.5, 1, 1] }}
-                    transition={{
-                        duration: 2,
-                        delay: 1,
-                        ease: "easeInOut",
-                        times: [0, 0.2, 0.4, 0.6, 0.8, 1]
-                    }}
-                    />
+                <motion.img src={aboutMePhoto} alt="emerick-fouchard-about-me-developper-full-stack" />
                 <div className={styles.AboutMeDesc}>
                     <h2>Mieux me connaître</h2>
                     <h3>Fun fact : Fan et joueur invétéré de Rocket League depuis 2016</h3>
                     <p>Test</p>
                 </div>
             </motion.article>
-        </section>
+        </motion.section>
     );
 }
 
