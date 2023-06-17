6f861c09c9556ca2858174b0341579c0b8be4531
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-17 16:11:50 +0200

    Setup apparition details onHover Work

--- src/components/Works/SingleWork/SingleWork.jsx
+++ src/components/Works/SingleWork/SingleWork.jsx
@@ -1,18 +1,46 @@
 import styles from './SingleWork.module.css';
 import React from 'react';
+import {
+    motion,
+    useScroll,
+    useTransform,
+  } from "framer-motion";
 
 function SingleWork({ work }) {
-    //const backgroundImageStyle = { backgroundImage: `url(${work.imgUrl})` };
+    const variants = {
+        initial: {
+            y: 0,
+            opacity: 0
+        },
+        onHover: {
+            y: -5,
+            opacity: 1,
+            transition: {
+                type: "spring",
+                bounce: 0.4,
+                duration: 0.8
+              }
+        }
+    }
 
     return (
-        <li className={styles.WorkLi}>
+        <motion.li 
+            className={styles.WorkLi}
+            initial="initial"
+            whileHover="onHover"
+            >
             <img src={work.imgUrl} alt={`${work.title}, Emerick Fouchard Developper Full-Stack - ${work.year}`}/>
-            <div className={styles.WorkDetails}>
-                <h3>{work.title}</h3>
-                <p>{work.year}</p>
+            <motion.div
+                className={styles.WorkDetails}
+                variants={variants}
+                >
+                <div className={styles.DivTitleYear}>
+                    <h3 className={styles.TextTitleYear}>{work.title}</h3>
+                    <p className={styles.TextTitleYear}>{work.year}</p>
+                </div>
                 <p>{work.desc}</p>
-            </div>
-        </li>
+            </motion.div>
+        </motion.li>
     );
 }
 
