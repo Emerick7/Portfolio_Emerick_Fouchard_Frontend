b84a267158d9c2dec9d0a744a7c3dc5123063e56
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-19 23:43:13 +0200

    Création footer + CSS Stay Connected

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,57 @@
-import './Footer.module.css';
+import styles from './Footer.module.css';
+import { Link } from 'react-router-dom';
+import { APP_PATHS } from '../../utils/constants';
+import { motion } from 'framer-motion';
 
-function Footer() {
-    return
+function Footer({ cursorVariant, setCursorVariant }) {
+    const hoverEnter = () => setCursorVariant("hover");
+    const hoverLeave = () => setCursorVariant("default");
+
+    const onHover = {
+        scale: 1.1,
+        rotate: -5,
+        backgroundColor: '#FD7272'
+    }
+
+    const transitionOnHover = {
+        type: "spring",
+        stiffness: 400,
+        damping: 10
+    }
+
+    return (
+        <footer className={styles.Footer}>
+            <div className={styles.TopDivFooter}>
+                <Link to={APP_PATHS.HOME} className={styles.EmerickLogo} onMouseEnter={hoverEnter} onMouseLeave={hoverLeave}>
+                    <span>E</span>merick
+                </Link>
+                <ul>
+                    <motion.li whileHover={onHover} transition={transitionOnHover}>
+                        <Link to={APP_PATHS.PORTFOLIO} onMouseEnter={hoverEnter} onMouseLeave={hoverLeave}>
+                            Portfolio
+                        </Link>
+                    </motion.li>
+                    <motion.li whileHover={onHover} transition={transitionOnHover}>
+                        <Link to={APP_PATHS.RESUME} onMouseEnter={hoverEnter} onMouseLeave={hoverLeave}>
+                            Curriculum Vitae
+                        </Link>
+                    </motion.li>
+                    <motion.li whileHover={onHover} transition={transitionOnHover}>
+                        <Link to={APP_PATHS.SERVICES} onMouseEnter={hoverEnter} onMouseLeave={hoverLeave}>
+                            Services
+                        </Link>
+                    </motion.li>
+                </ul>
+                <p>© 2023 EMERICK. ALL RIGHTS RESERVED</p>
+            </div>
+            <div className={styles.BottomDivFooter}>
+                <div/>
+                <div/>
+                <div/>
+                <div/>
+            </div>
+        </footer>
+    );
 }
 
 export default Footer;
--- src/components/StayConnected/StayConnected.jsx
+++ src/components/StayConnected/StayConnected.jsx
@@ -2,13 +2,14 @@ import { Link } from 'react-router-dom';
 import styles from './StayConnected.module.css';
 import { motion } from 'framer-motion';
 import { SOCIAL_PATHS, CONTACT_PATHS } from '../../utils/constants';
-import WhatsAppLogo from '../../assets/whatsapp-logo.png';
-import InstagramLogo from '../../assets/instagram-logo.png';
+import WhatsAppLogo from '../../assets/whatsapp-logo.svg';
+import InstagramLogo from '../../assets/instagram-logo.svg';
 
-function StayConnected(cursorVariant, setCursorVariant) {
+function StayConnected({cursorVariant, setCursorVariant}) {
     const hoverEnter = () => setCursorVariant("hover");
     const hoverLeave = () => setCursorVariant("default");
 
+    //Logos animation
     const onHover = { scale: 1.2 }
     const transitionOnHover = {
         type: "spring",
@@ -17,19 +18,25 @@ function StayConnected(cursorVariant, setCursorVariant) {
     }
 
     return (
-        <aside>
+        <aside className={styles.AsideContainer}>
             <h4>Restons connectés</h4>
             <p>Besançon, France</p>
-            <Link to={CONTACT_PATHS.MAIL}>fouchardemerick@gmail.com</Link>
-            <div>
-                <Link to={CONTACT_PATHS.WHATSAPP} /*onMouseEnter={hoverEnter} onMouseLeave={hoverLeave}*/ target="_blank">
+            <Link
+                to={CONTACT_PATHS.MAIL}
+                onMouseEnter={hoverEnter}
+                onMouseLeave={hoverLeave}
+                >
+                fouchardemerick@gmail.com
+            </Link>
+            <div className={styles.ContactButtonsDiv}>
+                <Link to={CONTACT_PATHS.WHATSAPP} onMouseEnter={hoverEnter} onMouseLeave={hoverLeave} target="_blank">
                     <motion.img
                         src={WhatsAppLogo}
                         whileHover={onHover}
                         transition={transitionOnHover}
                     />
                 </Link>
-                <Link to={SOCIAL_PATHS.INSTAGRAM} /*onMouseEnter={hoverEnter} onMouseLeave={hoverLeave}*/ target="_blank">
+                <Link to={SOCIAL_PATHS.INSTAGRAM} onMouseEnter={hoverEnter} onMouseLeave={hoverLeave} target="_blank">
                     <motion.img
                         src={InstagramLogo}
                         whileHover={onHover}
