e875970131202386e80b7d62e60523b1f1249c0f
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-22 16:04:14 +0200

    Card about me finale + animations

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,6 +1,6 @@
 import styles from './AboutMe.module.css';
 import { motion } from 'framer-motion';
-import aboutMePhoto from '../../assets/about-me-photo.webp';
+import aboutMePhoto from '../../assets/about-me-photo-1.webp';
 
 function AboutMe() {
     const variants = {
@@ -19,6 +19,68 @@ function AboutMe() {
         }
       };
 
+    const variantsCardOverlay = {
+        initial: {
+          left: 0,
+        },
+        onHover: {
+          left: "-38vw",
+          transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8,
+            delay: 0.2
+          }
+        }
+    }
+
+    const variantCardDesc = {
+        initial: {
+          left: 0,
+          color: "#CAD3C8",
+        },
+        onHover: {
+          left: "38vw",
+          color: "#3B3B98",
+          transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8,
+            delay: 0.4
+          }
+        }
+    }
+
+    /*const variantCardImg = {
+        initial: {
+          filter: "brightness(100%)"
+        },
+        onHover: {
+          filter: "brightness(100%)",
+          transition: {
+            type: "spring",
+            bounce: 0.4,
+            duration: 0.8,
+            delay: 0.4
+          }
+        }
+    }*/
+
+    const variantCardDots = {
+      initial: {
+        left: "16vw"
+      },
+      onHover: {
+        left: "54vw",
+        transition: {
+          type: "spring",
+          bounce: 0.4,
+          duration: 0.8,
+          delay: 0.4
+        }
+      }
+    }
+
     return (
         <motion.section
             className={styles.AboutMeSection}
@@ -26,30 +88,51 @@ function AboutMe() {
             whileInView="onscreen"
             viewport={{ once: true, amount: 0.8 }}
             >
-            <div className={styles.Splash}></div>
+            <div className={styles.SplashWallet}></div>
             <motion.article
-                className={styles.AboutMeBox}
+                className={styles.AboutMeCard}
                 variants={variants}
             >
-              <div>
-                <motion.img src={aboutMePhoto} alt="emerick-fouchard-about-me-developper-full-stack" />
-                <div className={styles.AboutMeDesc}>
-                    <h2>Emerick Fouchard</h2>
-                    <h3>Métier : Développeur Full-Stack JavaScript</h3>
-                    <h3>Lieu : Besançon, France</h3>
-                    <h3>Particularité : Voyage régulierement à Shillong, Inde</h3>
-                    <h3>Fun fact : Fan et joueur invétéré de Rocket League depuis 2016</h3>
-                </div>
-              </div>
-              <p>
-                Je suis un Développeur Full-Stack junior passionné par la création d'applications web et autres applications depuis mon adolescence. 
-                Mon épouse étant Indienne, nous sommes toujours entre la France et l'Inde. Mon métier me permet désormais de pouvoir travailler de n'importe où.
-                <br/><br/>
-                Diplômé avec une solide formation et une expérience pratique dans le développement front-end et back-end, 
-                je suis constamment motivé par le désir d'apprendre de nouvelles technologies et de relever des défis. 
-                Mon objectif est de créer des produits numériques qui offrent une expérience utilisateur exceptionnelle 
-                et qui répondent aux besoins spécifiques de mes clients.
-              </p>
+              <motion.div
+                className={styles.Div}
+                initial="initial"
+                whileHover="onHover"
+                >
+                <motion.div
+                  variants={variantsCardOverlay}
+                  >
+                  <motion.div 
+                    className={styles.AboutMeDesc}
+                    variants={variantCardDesc}
+                    >
+                      <h2>Emerick Fouchard</h2>
+                      <h3>Métier : Développeur Full-Stack JavaScript</h3>
+                      <h3>Lieu : Besançon, France</h3>
+                      <h3>Particularité : Voyage régulierement à Shillong, Inde</h3>
+                      <h3>Fun fact : Fan et joueur invétéré de Rocket League depuis 2016</h3>
+                  </motion.div>
+                  <motion.img
+                    src={aboutMePhoto}
+                    alt="emerick-fouchard-about-me-developper-full-stack"
+                    //variants={variantCardImg}
+                    />
+                  <motion.div
+                    variants={variantCardDots}>
+                    <div/>
+                    <div/>
+                    <div/>
+                  </motion.div>
+                </motion.div>
+                <p>
+                  Je suis un Développeur Full-Stack junior passionné par la création d'applications web et autres applications depuis mon adolescence. 
+                  Mon épouse étant Indienne, nous sommes toujours entre la France et l'Inde. Mon métier me permet désormais de pouvoir travailler de n'importe où.
+                  <br/><br/>
+                  Diplômé avec une solide formation et une expérience pratique dans le développement front-end et back-end, 
+                  je suis constamment motivé par le désir d'apprendre de nouvelles technologies et de relever des défis. 
+                  Mon objectif est de créer des produits numériques qui offrent une expérience utilisateur exceptionnelle 
+                  et qui répondent aux besoins spécifiques de mes clients.
+                </p>
+              </motion.div>
             </motion.article>
         </motion.section>
     );
