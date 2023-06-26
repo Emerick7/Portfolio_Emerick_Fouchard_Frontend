2d3d377964e064dea91b3fa25d300837c315bed3
Emerick Fouchard <fouchardemerick@gmail.com>
2023-06-26 21:38:00 +0200

    Ajout social buttons page portfolio

--- src/pages/Works/Works.jsx
+++ src/pages/Works/Works.jsx
@@ -3,13 +3,17 @@ import Footer from '../../components/Footer/Footer';
 import Header from '../../components/Header/Header';
 import WorksPortfolio from '../../components/WorksPortfolio/WorksPortfolio';
 import styles from './Works.module.css';
+import SocialButtons from '../../components/SocialButtons/SocialButtons';
 
 function Works({ cursorVariant, setCursorVariant }) {
     return(
         <React.StrictMode>
             <div className={styles.PageContainer}>
                 <Header cursorVariant={cursorVariant} setCursorVariant={setCursorVariant} />
-                <WorksPortfolio cursorVariant={cursorVariant} setCursorVariant={setCursorVariant} />
+                <div className={styles.ContentDiv}>
+                    <SocialButtons cursorVariant={cursorVariant} setCursorVariant={setCursorVariant} />
+                    <WorksPortfolio cursorVariant={cursorVariant} setCursorVariant={setCursorVariant} />
+                </div>
             </div>
             <Footer cursorVariant={cursorVariant} setCursorVariant={setCursorVariant} />
         </React.StrictMode>
