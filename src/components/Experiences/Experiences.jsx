9aecdb7bf1886ffbec34f84f25b98ff819d24b93
Emerick Fouchard <fouchardemerick@gmail.com>
2023-07-01 21:44:51 +0200

    Page Resume Terminée

--- src/components/Experiences/Experiences.jsx
+++ src/components/Experiences/Experiences.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import styles from './Experiences.module.css';
 import { motion } from 'framer-motion';
 
@@ -5,35 +6,42 @@ function Experiences() {
 
 
     return (
-        <article className={styles.ArticleExp}>
-            <section>
-                <h3>Magasinier (2018 - 2019)</h3>
-            </section>
-            <section>
-                <h3>Professeur particulier de français (2020 - 2021)</h3>
-            </section>
-            <section>
-                <h3>Youtube (choix du contenu, montage, gestion des vidéos) (2021 - 2021)</h3>
-            </section>
-            <section>
-                <h3>Développeur WordPress, réalisation et maintenance (2020)</h3>
-            </section>
-            <section>
-                <h3>Consultant en référencement SEO (2022)</h3>
-            </section>
-            <section>
-                <h3>Développeur Web Full-Stack JavaScript (2023)</h3>
-            </section>
-            <div className={styles.Timeline}>
-                <div className={styles.Dot}/>
-                <div className={styles.Dot}/>
-                <div className={styles.Dot}/>
-                <div className={styles.Dot}/>
-                <div className={styles.Dot}/>
-                <div className={styles.Dot}/>
-                <div className={styles.Line}/>
+        <div className={styles.ExpContainer}>
+            <div className={styles.WorksTop}>
+                <h2>Expérience professionnelle</h2>
+                <h3>Une timeline de mes expériences professionnelles marquantes</h3>
+                <hr className={styles.Separator}/>
             </div>
-        </article>
+            <article className={styles.ArticleExp}>
+                <section>
+                    <h4>Magasinier (2018 - 2019)</h4>
+                </section>
+                <section>
+                    <h4>Professeur particulier de français (2020 - 2021)</h4>
+                </section>
+                <section>
+                    <h4>Youtube (choix du contenu, montage, gestion des vidéos) (2021 - 2021)</h4>
+                </section>
+                <section>
+                    <h4>Développeur WordPress, réalisation et maintenance (2020)</h4>
+                </section>
+                <section>
+                    <h4>Consultant en référencement SEO (2022)</h4>
+                </section>
+                <section>
+                    <h4>Développeur Web Full-Stack JavaScript (2023)</h4>
+                </section>
+                <div className={styles.Timeline}>
+                    <div className={styles.Dot}/>
+                    <div className={styles.Dot}/>
+                    <div className={styles.Dot}/>
+                    <div className={styles.Dot}/>
+                    <div className={styles.Dot}/>
+                    <div className={styles.Dot}/>
+                    <div className={styles.Line}/>
+                </div>
+            </article>
+        </div>
     );
 }
 
