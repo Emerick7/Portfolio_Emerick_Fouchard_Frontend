d45d459b7217e24c2a99cde28d84974b88df01db
Emerick Fouchard <fouchardemerick@gmail.com>
2023-08-11 17:47:13 +0200

    Correction/Ajout framermotion reveal nth-child(2n)

--- src/components/ServicePageContent/ServicePageContent.jsx
+++ src/components/ServicePageContent/ServicePageContent.jsx
@@ -7,67 +7,97 @@ function ServicePageContent({ service, cursorVariant, setCursorVariant }) {
         return n % 2 === 0;
     }
 
-    const variantsText = {
-        offscreen: {
-            x: +200,
-            opacity: 0
-        },
-        onscreen: {
-            x: 0,
-            opacity: 1,
-            transition: {
-                type: "spring",
-                duration: 1,
-                delay: 0
-            }
-        }
-    };
-
-    const variantsImg = {
-        offscreen: {
-            x: -200,
-            opacity: 0
-        },
-        onscreen: {
-            x: 0,
-            opacity: 1,
-            transition: {
-                type: "spring",
-                duration: 1,
-                delay: 0
-            }
-        }
-    };
-
     return(
         <React.StrictMode>
             <div className={styles.H1Div} style={{background: `linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(${service.cover})`}}>
                 <h1>{service.h1}</h1>
             </div>
             <article>
-                {service.content.map((section, index) => (
-                    <motion.section
-                        key={index}
-                        className={styles.SectionArticle}
-                        initial="offscreen"
-                        whileInView="onscreen"
-                        viewport={{ once: true, amount: 0.8 }}
-                        variantsX={checkIndexIsEven(index) ? [+200, -200] : [-200, +200]}>
-                        <div className={styles.ContentDiv}>
-                            <motion.div
-                                variants={variantsText}>
-                                { section.h2 ? <h2>{section.h2}</h2>: null }
-                                { section.h3 ? <h3>{section.h3}</h3>: null }
-                                { section.paragraph ? <p>{section.paragraph}</p>: null }
-                            </motion.div>
-                            { section.image ? 
-                                <motion.div className={styles.ImgDiv} variants={variantsImg}>
-                                    <img src={section.image} alt={section.imgAlt}/>
-                                </motion.div> : null 
-                            }
-                        </div>
-                    </motion.section>
-                ))}
+                {service.content.map((section, index) => {
+                    let variants = [];
+
+                    return (
+                        <motion.section
+                            key={index}
+                            className={styles.SectionArticle}
+                            initial="offscreen"
+                            whileInView="onscreen"
+                            viewport={{ once: true, amount: 0.8 }}
+                            variantsX={checkIndexIsEven(index) ? variants=[{
+                                offscreen: {
+                                    x: -200,
+                                    opacity: 0
+                                },
+                                onscreen: {
+                                    x: 0,
+                                    opacity: 1,
+                                    transition: {
+                                        type: "spring",
+                                        duration: 1,
+                                        delay: 0
+                                    }
+                                }
+                            },
+                            {
+                                offscreen: {
+                                    x: 200,
+                                    opacity: 0
+                                },
+                                onscreen: {
+                                    x: 0,
+                                    opacity: 1,
+                                    transition: {
+                                        type: "spring",
+                                        duration: 1,
+                                        delay: 0
+                                    }
+                                }
+                            }] : variants=[{
+                                offscreen: {
+                                    x: 200,
+                                    opacity: 0
+                                },
+                                onscreen: {
+                                    x: 0,
+                                    opacity: 1,
+                                    transition: {
+                                        type: "spring",
+                                        duration: 1,
+                                        delay: 0
+                                    }
+                                }
+                            },
+                            {
+                                offscreen: {
+                                    x: -200,
+                                    opacity: 0
+                                },
+                                onscreen: {
+                                    x: 0,
+                                    opacity: 1,
+                                    transition: {
+                                        type: "spring",
+                                        duration: 1,
+                                        delay: 0
+                                    }
+                                }
+                            }]}>
+                            <div className={styles.ContentDiv}>
+                                <motion.div
+                                    variants={variants[0]}>
+                                    { section.h2 ? <h2>{section.h2}</h2>: null }
+                                    { section.h3 ? <h3>{section.h3}</h3>: null }
+                                    { section.paragraph ? <p>{section.paragraph}</p>: null }
+                                </motion.div>
+                                { section.image ? 
+                                    <motion.div className={styles.ImgDiv} variants={variants[1]}>
+                                        <img src={section.image} alt={section.imgAlt}/>
+                                    </motion.div> : null 
+                                }
+                            </div>
+                        </motion.section>
+                    )}
+                )}
             </article>
         </React.StrictMode>
     );
